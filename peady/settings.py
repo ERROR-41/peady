59c76ac3fd9f5c49c10a0a64c846e3168463f54f
md.omar <mdomarsadek41@gmail.com>
2025-05-18 19:32:08 +0600

    setup whitenoise

--- peady/settings.py
+++ peady/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "cloudinary",
     "cloudinary_storage",
+    "whitenoise.runserver_nostatic",
     "drf_yasg",
     "rest_framework",
     "django_filters",
